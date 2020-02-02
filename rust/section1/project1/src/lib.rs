353f3f6be580c10425fedadfa236c2aaaaeb8b4a
ZiheLiu <272910663@qq.com>
2020-02-02 12:55:26 +0800

    section1:project1: make the tests compile

--- rust/section1/project1/src/lib.rs
+++ rust/section1/project1/src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub struct KvStore {
+
+}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!();
+    }
+
+    pub fn get(&mut self, key: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!();
     }
 }
